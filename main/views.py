e37a27e05be73dc2801ce912bedbabf2e1b749b4
oorgrim <oorgrim@gmail.com>
2024-12-12 01:23:06 +0500

    10.12

--- main/views.py
+++ main/views.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
+from django.template.exceptions import TemplateDoesNotExist
+from django.template.loader import get_template
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def other_page(request, page):
+    try:
+        template = get_template('main/' + page + '.html')
+    except TemplateDoesNotExist:
+        raise Http404
+    return HttpResponse(template.render(request=request))
+
+
+class BBLoginView(LoginView):
+    template_name = 'main/login.html'
+
+
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html')
